12e0fdfe7120b301520af1cb99967ebe19899a56
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2023-04-06 21:06:10 +0900

    アカウント操作プログラム(サンプル)
    作成したアカウントを使用して一斉に操作を行うことが出来るプログラムです。
    あくまでもサンプルでフォローしか出来ません

---
+++ bot.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const chalk = require('chalk');
+const pLimit = require('p-limit');
+const retry = require('async-retry');
+const fs = require('fs');
+
+const proxy = {
+    protocol: 'http:',
+    hostname: '127.0.0.1',
+    port: 80,
+    auth: {
+        username: 'username',
+        password: 'password'
+    }
+}
+
+const target = '1';
+
+const follow = async (i) => {
+    const account = await fs.promises.readFile(`accounts/${i}`).then(JSON.parse);
+    const response = await axios.post(
+        `https://mstdn.jp/api/v1/accounts/${target}/follow`,
+        {
+            'reblogs': true
+        },
+        {
+            headers: {
+                'user-agent': 'MastodonAndroid/1.1.3',
+                'authorization': `Bearer ${account.access_token}`,
+                'content-type': 'application/json;charset=utf-8'
+            },
+            proxy: proxy,
+            validateStatus: (status) => status < 500
+        }
+    );
+    if (response.status === 200) {
+        console.log(`${account.username}: ${chalk.green(response.status)}`);
+    } else {
+        console.log(`${account.username}: ${chalk.red(response.status)}`);
+    }
+}
+
+const main = async () => {
+    const accounts = await fs.promises.readdir('accounts');
+    const limit = pLimit(50);
+    await Promise.all(accounts.map((i) => limit(() => retry(() => follow(i)))));
+}
+
+main();
